8ce256a55b0f4baf2a54e510047c40e64c854778
Jiri Kuncar <jiri.kuncar@gmail.com>
2017-10-25 15:44:31 +0200

    docker: publish rengahub/minimal-notebook
    (closes #56) (addresses #40)

--- .travis/deploy-docker.sh
+++ .travis/deploy-docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
-docker build -t ${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-deployer:latest .
-docker push ${DOCKER_REGISTRY}${DOCKER_REPOSITORY}renga-deployer:latest
+docker build -t ${DOCKER_REGISTRY}${DOCKER_REPOSITORY}minimal-notebook:latest .
+docker push ${DOCKER_REGISTRY}${DOCKER_REPOSITORY}minimal-notebook:latest
