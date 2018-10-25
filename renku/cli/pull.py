b18724fe06fb7ada55f7ba1844e6aaf165f5da76
Rok Roskar <roskarr@ethz.ch>
2018-10-25 21:58:54 +0200

    cli: simplify lfs pull

--- renku/cli/pull.py
+++ renku/cli/pull.py
@@ -15,7 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Pull data from LFS."""
+"""Pull data from external storage."""
 
 import click
 
@@ -24,7 +24,7 @@ from ._client import pass_local_client
 
 @click.group()
 def pull():
-    """Pull data from LFS."""
+    """Pull data from external storage."""
 
 
 @pull.command()
@@ -35,47 +35,7 @@ def pull():
     required=True,
 )
 @pass_local_client
-def file(client, filename):
-    """Create an empty dataset in the current repo."""
-    from pathlib import Path
-
-    # handle a symlink
-    # 1. check if the filename is a symlink
-    # 2. if yes, check if it is inside a submodule
-    # 3. if no, make sure the target is inside the current project and pull
-    # 4. if in a submodule, go there and pull
-
-    p = Path(filename)
-    if p.is_symlink():
-        p = p.resolve()
-        submodule_path = in_submodule(p, client.git)
-        if submodule_path:
-            lfs_pull_file(p.relative_to(submodule_path), cwd=submodule_path)
-
-        else:
-            # TODO: check if the file is inside the project
-            lfs_pull_file(
-                p.relative_to(Path(client.git.working_dir).absolute()),
-                cwd=client.git.working_dir
-            )
-
-    else:
-        lfs_pull_file(p)
-
-
-def in_submodule(path, repo):
-    """Check if a path is inside a submodule."""
-    from pathlib import Path
-    for submodule in repo.submodules:
-        submodule_path = Path(submodule.path).absolute()
-        if str(path).startswith(str(submodule_path)):
-            return submodule_path
-
-
-def lfs_pull_file(path, cwd=None):
-    """Pull a path from LFS."""
-    import subprocess
-    subprocess.run(['git', 'lfs', 'pull', 'origin', '-I',
-                    str(path)],
-                   cwd=cwd,
-                   check=True)
+def path(client, paths):
+    """Pull the specified path from external storage."""
+    for p in paths:
+        client.pull_path(p)
