1b637dfec606f9d3149dfb2e54165c79dfae4936
Rok Roskar <rok.roskar@sdsc.ethz.ch>
2019-08-28 23:50:07 +0200

    fix: set the blank node correctly

--- renku/models/datasets.py
+++ renku/models/datasets.py
@@ -128,13 +128,13 @@ class Creator(object):
         """Set the default id."""
         if self.email:
             return 'mailto:{email}'.format(email=self.email)
-        return '_' + str(uuid.uuid4())
+        return '_:{}'.format(str(uuid.uuid4()))
 
     def __attrs_post_init__(self):
         """Finish object initialization."""
         # handle the case where ids were improperly set
         if self._id == 'mailto:None':
-            self._id = '_' + str(uuid.uuid4())
+            self._id = self.default_id()
 
 
 @attr.s
@@ -321,7 +321,7 @@ class Dataset(Entity, CreatorsMixin):
     )
 
     description = jsonld.ib(
-        default=None, context='schema:description', kw_only=True
+        default='', context='schema:description', kw_only=True
     )
 
     identifier = jsonld.ib(
