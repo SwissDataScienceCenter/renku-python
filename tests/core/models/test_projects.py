7b555e9d5cef5aa220b7d4e80223be36fa19263f
Mohammad Alisafaee <mohammad.alisafaee@epfl.ch>
2020-06-11 17:30:45 +0200

    Remove unnecessary tests

--- tests/core/models/test_projects.py
+++ tests/core/models/test_projects.py
@@ -16,77 +16,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Test projects API."""
-from datetime import datetime, timezone
+from datetime import timezone
 
-import pytest
-import yaml
 from freezegun import freeze_time
 
-from renku.core.models.jsonld import NoDatesSafeLoader
 from renku.core.models.projects import Project
-
-# Do not modify the content so we can ensure backwards compatibility.
-PROJECT_V1 = """
-'@context':
-  created: http://schema.org/dateCreated
-  foaf: http://xmlns.com/foaf/0.1/
-  name: foaf:name
-  updated: http://schema.org/dateUpdated
-  version: http://schema.org/schemaVersion
-'@type': foaf:Project
-created: 2018-02-22 10:40:14.878009
-name: demo
-updated: 2018-02-22 15:33:11.089204
-version: '1'
-"""
-
-PROJECT_V2 = """
-'@context':
-  _id: '@id'
-  created: schema:dateCreated
-  creator: schema:creator
-  name: schema:name
-  prov: http://www.w3.org/ns/prov#
-  schema: http://schema.org/
-  updated: schema:dateUpdated
-  version: schema:schemaVersion
-'@type':
-- prov:Location
-- schema:Project
-_id: https://renku.ch/demo/demo
-created: '2019-09-18T15:18:16.866609+00:00'
-creator:
-  '@context':
-    _id: '@id'
-    affiliation: schema:affiliation
-    alternate_name: schema:alternateName
-    email: schema:email
-    name: schema:name
-    schema: http://schema.org/
-  '@type': schema:Person
-  _id: mailto:demo@example.com
-  affiliation: null
-  alternate_name: null
-  email: demo@example.com
-  name: demo user
-name: demo
-updated: '2019-09-18T15:18:16.868838+00:00'
-version: 2
-"""
-
-PROJECT_V1_BROKEN = """
-'@context':
-  created: http://schema.org/dateCreated
-  foaf: http://xmlns.com/foaf/0.1/
-  name: notexisting:name
-  updated: http://schema.org/dateUpdated
-  version: http://schema.org/schemaVersion
-'@type': foaf:Project
-created: 2018-02-22 10:40:14.878009
-name: demo
-updated: 2018-02-22 15:33:11.089204
-version: '1'
-"""
 
 
 def test_project_serialization():
@@ -111,42 +45,6 @@ def test_project_serialization():
         str(SUPPORTED_PROJECT_VERSION) ==
         data['http://schema.org/schemaVersion']
     )
-
-
-@pytest.mark.parametrize(
-    'project_meta,version,is_broken', [
-        (PROJECT_V1, 1, False),
-        (PROJECT_V1_BROKEN, 1, True),
-        (PROJECT_V2, 2, False),
-    ]
-)
-def test_project_metadata_compatibility(project_meta, version, is_broken):
-    """Test loading of the initial version."""
-    project = Project.from_jsonld(
-        yaml.load(project_meta, Loader=NoDatesSafeLoader)
-    )
-
-    assert str(version) == project.version
-
-    if not is_broken:
-        assert 'demo' == project.name
-
-    assert project.updated is not None
-    assert project.created is not None
-
-
-@pytest.mark.parametrize('project_meta', [PROJECT_V1, PROJECT_V2])
-def test_project_datetime_loading(project_meta):
-    """Check that datetime is correctly loaded."""
-    project = Project.from_jsonld(
-        yaml.load(project_meta, Loader=NoDatesSafeLoader)
-    )
-
-    assert isinstance(project.updated, datetime)
-    assert isinstance(project.created, datetime)
-
-    assert project.updated.tzinfo is not None
-    assert project.created.tzinfo is not None
 
 
 def test_project_creator_deserialization(client, project):
