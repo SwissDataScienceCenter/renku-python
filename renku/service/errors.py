6f805e60df51ce6ddca0dc4a968dae522db5bbb8
Sam <contact@justsam.io>
2020-06-24 13:42:38 +0200

    chore(service): raise correct exception on missing reference (#1342)

---
+++ renku/service/errors.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017-2020 - Swiss Data Science Center (SDSC)
+# A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+# Eidgenössische Technische Hochschule Zürich (ETHZ).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Renku service exceptions."""
+from renku.core.errors import RenkuException
+
+
+class ProjectNotFound(RenkuException):
+    """Project reference not found exception."""
+
+    def __init__(self, project_id):
+        """Build a custom message."""
+        message = (f'project_id "{project_id}" not found')
+        super(ProjectNotFound, self).__init__(message)
