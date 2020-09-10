73cab03f7ab5b5a4b299b35fab39dc95a5379ae9
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2020-09-10 17:06:21 +0200

    chore: add Flake8 test

--- run-tests.sh
+++ run-tests.sh
@@ -39,6 +39,7 @@ check_styles(){
     pydocstyle renku tests conftest.py docs
     black --check --diff renku tests conftest.py
     isort -c --df .
+    flake8 renku tests conftest.py setup.py
     check-manifest --ignore ".travis-*,renku/version.py,renku/templates,renku/templates/**"
     find . -path ./.eggs -prune -o -iname \*.sh -print0 | xargs -0 shellcheck
 }
