478f08c4388f65a4369c7f5119897ee060cece26
Mohammad Alisafaee <50210674+mohammad-sdsc@users.noreply.github.com>
2020-10-12 18:59:20 +0200

    fix(dataset): remove blank nodes (#1602)

--- tests/cli/test_integration_datasets.py
+++ tests/cli/test_integration_datasets.py
@@ -305,7 +305,7 @@ def test_dataset_import_renkulab_dataset(runner, project, client, url):
 
 
 @pytest.mark.integration
-@flaky(max_runs=1, min_passes=1)
+@flaky(max_runs=10, min_passes=1)
 def test_import_renku_dataset_preserves_directory_hierarchy(runner, project, client):
     """Test dataset imported from Renku projects have correct directory hierarchy."""
     url = "https://dev.renku.ch/datasets/1a637fd1-a7a6-4d1f-b9aa-157e7033cd1c"
