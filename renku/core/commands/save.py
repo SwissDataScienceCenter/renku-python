b88f2cfeb709eff7cd135c540916ee7e135ced8c
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2021-01-25 09:28:16 +0100

    address PR comments

--- renku/core/commands/save.py
+++ renku/core/commands/save.py
@@ -93,7 +93,9 @@ def repo_sync(repo, message=None, remote=None, paths=None):
     if paths:
         # commit uncommitted changes
         try:
-            staged_files = {d.a_path for d in repo.index.diff("HEAD")} if repo.head.is_valid() else set()
+            staged_files = (
+                {git_unicode_unescape(d.a_path) for d in repo.index.diff("HEAD")} if repo.head.is_valid() else set()
+            )
 
             path_to_save = set(paths) - staged_files
 
