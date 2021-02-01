8e35a0fefeef5e4e61ec1b5184981ef069a48d97
Mohammad Alisafaee <mohammad.alisafaee@epfl.ch>
2021-02-01 16:33:14 +0100

    refactor(core): unified graph generation; shacl validation (#1851)

--- renku/cli/graph.py
+++ renku/cli/graph.py
@@ -17,15 +17,12 @@
 # limitations under the License.
 """PoC command for testing the new graph design."""
 
-import functools
-import sys
-
 import click
 
 from renku.cli.utils.callback import ClickCallback
 from renku.cli.utils.click import CaseInsensitiveChoice
 from renku.core.incubation.command import Command
-from renku.core.incubation.graph import export_graph, generate_datasets_provenance, generate_graph
+from renku.core.incubation.graph import FORMATS, export_graph, generate_graph
 from renku.core.incubation.graph import status as get_status
 from renku.core.incubation.graph import update as perform_update
 from renku.core.models.workflow.dependency_graph import DependencyGraph
@@ -122,62 +119,13 @@ def save(path):
         Command().command(_to_png).build().execute(path=path)
 
 
-def _dot(rdf_graph, simple=True, debug=False, landscape=False):
-    """Format graph as a dot file."""
-    from rdflib.tools.rdf2dot import rdf2dot
-
-    from renku.core.commands.format.graph import _rdf2dot_reduced, _rdf2dot_simple
-
-    if debug:
-        rdf2dot(rdf_graph, sys.stdout)
-        return
-
-    sys.stdout.write('digraph { \n node [ fontname="DejaVu Sans" ] ; \n ')
-    if landscape:
-        sys.stdout.write('rankdir="LR" \n')
-    if simple:
-        _rdf2dot_simple(rdf_graph, sys.stdout, graph=graph)
-        return
-    _rdf2dot_reduced(rdf_graph, sys.stdout)
-
-
-_dot_full = functools.partial(_dot, simple=False, landscape=False)
-_dot_landscape = functools.partial(_dot, simple=True, landscape=True)
-_dot_full_landscape = functools.partial(_dot, simple=False, landscape=True)
-_dot_debug = functools.partial(_dot, debug=True)
-
-
-def _json_ld(rdf_graph):
-    """Format graph as JSON-LD."""
-    data = rdf_graph.serialize(format="json-ld").decode("utf-8")
-    print(data)
-
-
-_FORMATS = {
-    "dot": _dot,
-    "dot-full": _dot_full,
-    "dot-landscape": _dot_landscape,
-    "dot-full-landscape": _dot_full_landscape,
-    "dot-debug": _dot_debug,
-    "json-ld": _json_ld,
-    "jsonld": _json_ld,
-}
-
-
 @graph.command()
-@click.option("--format", type=CaseInsensitiveChoice(_FORMATS), default="json-ld", help="Choose an output format.")
-@click.option("-d", "--dataset", is_flag=True, help="Include datasets.")
-def export(format, dataset):
+@click.option("--format", type=CaseInsensitiveChoice(FORMATS), default="json-ld", help="Choose an output format.")
+@click.option("--strict", is_flag=True, default=False, help="Validate triples before output.")
+@click.option("--workflows-only", is_flag=True, help="Exclude datasets metadata from export.")
+def export(format, strict, workflows_only):
     r"""Equivalent of `renku log --format json-ld`."""
     communicator = ClickCallback()
-    (export_graph().with_communicator(communicator).build().execute(format=_FORMATS[format], dataset=dataset))
-
-
-@graph.command()
-@click.option("-f", "--force", is_flag=True, help="Delete existing metadata and regenerate all.")
-def generate_dataset(force):
-    """Create new graph metadata for datasets."""
-    communicator = ClickCallback()
-    (generate_datasets_provenance().with_communicator(communicator).build().execute(force=force))
-
-    click.secho("\nOK", fg="green")
+    export_graph().with_communicator(communicator).build().execute(
+        format=format, workflows_only=workflows_only, strict=strict
+    )
