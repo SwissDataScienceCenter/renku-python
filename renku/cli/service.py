8d0ce07be227b7b06fbe8ebf5b9e7da3b605aa1f
sam <contact@justsam.io>
2021-03-08 11:15:12 +0100

    test: macos

--- renku/cli/service.py
+++ renku/cli/service.py
@@ -101,7 +101,7 @@ def is_renku_process(process, include):
     """Return true if this is a renku process."""
     process_name = process.name().lower()
 
-    if process_name == "renku" and check_cmdline(process.cmdline(), include):
+    if process_name == "renku":
         return True
     elif process_name != "python":
         return False
