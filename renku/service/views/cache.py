68a3087ba16ab877c65807acefe71ce0432fc886
Rok Roskar <rok.roskar@sdsc.ethz.ch>
2021-05-12 15:31:46 +0200

    chore(svc): update upload file view doc

--- renku/service/views/cache.py
+++ renku/service/views/cache.py
@@ -75,10 +75,18 @@ def upload_file_view(user_data, cache):
     ---
     post:
       description: Upload a file or archive of files.
+      parameters:
+        - in: query
+          schema: FileUploadRequest
       requestBody:
         content:
-          application/json:
-            schema: FileUploadRequest
+          multipart/form-data:
+            schema:
+              type: object
+              properties:
+                file:
+                  type: string
+                  format: binary
       responses:
         200:
           description: List of uploaded files.
