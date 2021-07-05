eb37031f6391dbc2ba134c3d780555d76bf23a10
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2021-07-05 15:37:58 +0200

    update documentation

--- renku/service/serializers/cache.py
+++ renku/service/serializers/cache.py
@@ -254,43 +254,75 @@ class ProjectMigrationCheckRequest(Schema):
 class ProjectCompatibilityResponse(Schema):
     """Response schema outlining service compatibility for migrations check."""
 
-    project_metadata_version = fields.String()
-    current_metadata_version = fields.String()
-    migration_required = fields.Boolean()
+    project_metadata_version = fields.String(description="Current version of the Renku metadata in the project.")
+    current_metadata_version = fields.String(description="Highest metadata version supported by this service.")
+    migration_required = fields.Boolean(
+        description="Whether or not a metadata migration is required to be compatible with this service."
+    )
 
 
 class DockerfileStatusResponse(Schema):
     """Response schema outlining dockerfile status for migrations check."""
 
-    newer_renku_available = fields.Boolean()
-    automated_dockerfile_update = fields.Boolean()
-    latest_renku_version = fields.String()
-    dockerfile_renku_version = fields.String()
+    newer_renku_available = fields.Boolean(
+        description="Whether the version of Renku un this service is newer than the one in the Dockerfile."
+    )
+    automated_dockerfile_update = fields.Boolean(
+        description="Whether or not the Dockerfile supports automated Renku version updates."
+    )
+    latest_renku_version = fields.String(description="The current version of Renku available in this service.")
+    dockerfile_renku_version = fields.String(description="Version of Renku specified in the Dockerfile.")
 
 
 class TemplateStatusResponse(Schema):
     """Response schema outlining template status for migrations check."""
 
-    automated_template_update = fields.Boolean()
-    newer_template_available = fields.Boolean()
-    template_source = fields.String()
-    template_ref = fields.String()
-    template_id = fields.String()
+    automated_template_update = fields.Boolean(
+        description="Whether or not the project template explicitely supports automated updates."
+    )
+    newer_template_available = fields.Boolean(
+        description=(
+            "Whether or not the current version of the project template differs from the " "one used in the project."
+        )
+    )
+    template_source = fields.String(
+        description="Source of the template repository, either a Git URL or 'renku' if an embedded template was used."
+    )
+    template_ref = fields.String(
+        description="The branch/tag/commit from the template_source repository that was used to create this project."
+    )
+    template_id = fields.String(description="The id of the template in the template repository.")
 
-    project_template_version = fields.String(allow_none=True)
-    latest_template_version = fields.String(allow_none=True)
+    project_template_version = fields.String(
+        allow_none=True, description="The version of the template last used in the user's project."
+    )
+    latest_template_version = fields.String(
+        allow_none=True, description="The current version of the template in the template repository."
+    )
 
 
 class ProjectMigrationCheckResponse(Schema):
     """Response schema for project migration check."""
 
-    project_supported = fields.Boolean()
-    core_renku_version = fields.String()
-    project_renku_version = fields.String()
+    project_supported = fields.Boolean(
+        description=(
+            "Determines whether this project is a Renku project that is supported by the version "
+            "running on this service (not made with a newer version)."
+        )
+    )
+    core_renku_version = fields.String(description="Version of Renku running in this service.")
+    project_renku_version = fields.String(description="Version of Renku last used to change the project.")
 
-    core_compatibility_status = fields.Nested(ProjectCompatibilityResponse)
-    dockerfile_renku_status = fields.Nested(DockerfileStatusResponse)
-    template_status = fields.Nested(TemplateStatusResponse)
+    core_compatibility_status = fields.Nested(
+        ProjectCompatibilityResponse,
+        description="Fields detailing the compatibility of the project with this core service version.",
+    )
+    dockerfile_renku_status = fields.Nested(
+        DockerfileStatusResponse, description="Fields detailing the status of the Dockerfile in the project."
+    )
+    template_status = fields.Nested(
+        TemplateStatusResponse, description="Fields detailing the status of the project template used by this project."
+    )
 
 
 class ProjectMigrationCheckResponseRPC(JsonRPCResponse):
