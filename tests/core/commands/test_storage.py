e82899faa66be64b7a86ca0b0bc9ade1de57ac3e
Mohammad Alisafaee <mohammad.alisafaee@epfl.ch>
2021-07-09 16:11:43 +0200

    feat(core): migration for new metadata

--- tests/core/commands/test_storage.py
+++ tests/core/commands/test_storage.py
@@ -16,7 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Storage command tests."""
-
 import os
 import subprocess
 from pathlib import Path
