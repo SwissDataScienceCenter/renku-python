292c223defcbdee59513ba68b0c5986fe176aa5f
Lorenzo Cavazzi <43481553+lorenzo-cavazzi@users.noreply.github.com>
2021-08-06 10:55:38 +0200

    improvement(core): allow longer commit messages (#2238)
    Allow writing longer commit messages that are wrapped and trimmed.
    
    re #2152

--- renku/core/utils/scm.py
+++ renku/core/utils/scm.py
@@ -19,6 +19,8 @@
 import re
 from functools import reduce
 
+from renku.core.errors import ParameterError
+
 
 def is_ascii(data):
     """Check if provided string contains only ascii characters."""
@@ -51,25 +53,41 @@ def git_unicode_unescape(s: str, encoding: str = "utf-8") -> str:
     return s
 
 
-def shorten_message(message, max_length=100, cut=True):
-    """Shortens or wraps a commit message to be at most `max_len` characters per line."""
+def shorten_message(message: str, line_length: int = 100, body_length: int = 65000):
+    """Wraps and shortens a commit message.
 
-    if len(message) < max_length:
+    :param message: message to adjust.
+    :param line_length: maximum line length before wrapping. 0 for infinite.
+    :param body_length: maximum body length before cut. 0 for infinite.
+    :return: message wrapped and trimmed.
+    """
+
+    if line_length < 0:
+        raise ParameterError("the length can't be negative.", "line_length")
+
+    if body_length < 0:
+        raise ParameterError("the length can't be negative.", "body_length")
+
+    if body_length and len(message) > body_length:
+        message = message[: body_length - 3] + "..."
+
+    if line_length == 0 or len(message) <= line_length:
         return message
-
-    if cut:
-        return message[: max_length - 3] + "..."
 
     lines = message.split(" ")
     lines = [
         line
-        if len(line) < max_length
-        else "\n\t".join(line[o : o + max_length] for o in range(0, len(line), max_length))
+        if len(line) < line_length
+        else "\n\t".join(line[o : o + line_length] for o in range(0, len(line), line_length))
         for line in lines
     ]
 
-    return reduce(
-        lambda c, x: (f"{c[0]} {x}", c[1] + len(x) + 1) if c[1] + len(x) <= max_length else (f"{c[0]}\n\t" + x, len(x)),
+    # NOTE: tries to preserve message spacing.
+    wrapped_message = reduce(
+        lambda c, x: (f"{c[0]} {x}", c[1] + len(x) + 1)
+        if c[1] + len(x) <= line_length
+        else (f"{c[0]}\n\t" + x, len(x)),
         lines,
         ("", 0),
-    )
+    )[0]
+    return wrapped_message[1:]
