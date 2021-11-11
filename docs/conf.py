bb5c632d76470c8580f69fd7af5f4a7a31697360
Rok Roškar <rok.roskar@sdsc.ethz.ch>
2021-11-11 23:30:07 +0100

    chore(docs): add spellcheck

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,7 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
+    "sphinxcontrib.spelling",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
