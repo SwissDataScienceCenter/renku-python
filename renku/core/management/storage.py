cc7407ff1fce31a178d47fa96cfd0bf012607f62
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2021-12-02 13:24:52 +0100

    fix(core): fix regression due to git-lfs version 3.0.2 (#2493)

--- renku/core/management/storage.py
+++ renku/core/management/storage.py
@@ -450,15 +450,26 @@ class StorageApiMixin(RepositoryApiMixin):
 
         includes, excludes, above = self.get_lfs_migrate_filters()
 
+        ignore_pointers = ["--pointers", "ignore"]
+
         command = self._CMD_STORAGE_MIGRATE_INFO + ref + above + includes + excludes
 
         try:
-            lfs_output = run(command, stdout=PIPE, stderr=STDOUT, cwd=self.path, universal_newlines=True)
+            lfs_output = run(
+                command + ignore_pointers, stdout=PIPE, stderr=STDOUT, cwd=self.path, universal_newlines=True
+            )
         except (KeyboardInterrupt, OSError) as e:
             raise errors.GitError(f"Couldn't run 'git lfs migrate info':\n{e}")
 
         if lfs_output.returncode != 0:
-            raise errors.GitLFSError(f"Error executing 'git lfs migrate info: \n {lfs_output.stdout}")
+            # NOTE: try running without --pointers (old versions of git lfs)
+            try:
+                lfs_output = run(command, stdout=PIPE, stderr=STDOUT, cwd=self.path, universal_newlines=True)
+            except (KeyboardInterrupt, OSError) as e:
+                raise errors.GitError(f"Couldn't run 'git lfs migrate info':\n{e}")
+
+            if lfs_output.returncode != 0:
+                raise errors.GitLFSError(f"Error executing 'git lfs migrate info: \n {lfs_output.stdout}")
 
         groups = []
         files_re = re.compile(r"(.*\s+[\d.]+\s+\S+).*")
