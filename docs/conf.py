7f54b8a03dff315e3b9fd933faf8dab72dc5494b
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2022-03-09 10:51:41 +0100

    Add sphinx napoleon doc style

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,7 @@ extensions = [
     "sphinx.ext.coverage",
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinxcontrib.spelling",
     "sphinx_tabs.tabs",
