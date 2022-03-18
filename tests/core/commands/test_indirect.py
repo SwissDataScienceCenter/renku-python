c83d400000eb9172e1239e1c97b4746d325af792
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2022-03-18 15:24:24 +0100

    feat(api): allow explicit input/output values to be set for workflow execution (#2728)

--- tests/core/commands/test_indirect.py
+++ tests/core/commands/test_indirect.py
@@ -36,7 +36,7 @@ def test_set_indirect_input_files_paths_via_env_var(tmp_path, env_var, reset_env
 
     assert path.is_absolute()
 
-    expected_path = Path(".renku") / "tmp" / env_var / "inputs.txt"
+    expected_path = Path(".renku") / "tmp" / env_var / "inputs.yml"
 
     assert expected_path == path.relative_to(tmp_path)
 
@@ -51,7 +51,7 @@ def test_set_indirect_output_files_paths_via_env_var(tmp_path, env_var, reset_en
 
     assert path.is_absolute()
 
-    expected_path = Path(".renku") / "tmp" / env_var / "outputs.txt"
+    expected_path = Path(".renku") / "tmp" / env_var / "outputs.yml"
 
     assert expected_path == path.relative_to(tmp_path)
 
