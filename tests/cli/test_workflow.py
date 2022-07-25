c85790b3d8bef6e60508629bbc60637c33a86365
Lorenzo Cavazzi <43481553+lorenzo-cavazzi@users.noreply.github.com>
2022-07-25 19:11:14 +0200

    feat(workflow): add dot output on workflow visualize (#3032)
    - Fixes also a bug when providing the wrong columns
    
    fix #2376
    
    Co-authored-by: Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>

--- tests/cli/test_workflow.py
+++ tests/cli/test_workflow.py
@@ -917,6 +917,24 @@ def test_workflow_visualize_non_interactive(runner, project, client, workflow_gr
     assert "H" in result.output
 
 
+def test_workflow_visualize_dot(runner, project, client, workflow_graph):
+    """Test renku workflow visualize dot format."""
+
+    result = runner.invoke(cli, ["workflow", "visualize", "--format", "dot", "--revision", "HEAD^", "H", "S"])
+
+    assert 0 == result.exit_code, format_result_exception(result)
+    assert '"Y" -> "bash -c \\"cat X Y | tee R S\\"";' in result.output
+    assert '"X" -> "bash -c \\"cat X Y | tee R S\\"";' in result.output
+    assert '"bash -c \\"cat X Y | tee R S\\"" -> "R";' in result.output
+    assert '"bash -c \\"cat X Y | tee R S\\"" -> "S";' in result.output
+    assert 4 == result.output.count('"bash -c \\"cat X Y | tee R S\\"')
+
+    assert 1 == result.output.count('"echo other > H" -> "H"')
+    assert 1 == result.output.count('-> "H"')
+    assert 0 == result.output.count('"H" -->')
+    assert 1 == result.output.count('"H"')
+
+
 @pytest.mark.skip(
     "Doesn't actually work, not really a tty available in github actions, "
     "see https://github.com/actions/runner/issues/241"
