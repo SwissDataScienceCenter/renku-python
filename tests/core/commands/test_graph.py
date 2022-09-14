c6f0d0824b9f003dc32ebe80495d237bd50172d5
Mohammad Alisafaee <mohammad.alisafaee@epfl.ch>
2022-09-14 12:58:47 +0200

    fix

--- tests/core/commands/test_graph.py
+++ tests/core/commands/test_graph.py
@@ -519,7 +519,7 @@ def test_graph_view_model():
 
     result = model.as_nt_string()
     assert isinstance(result, str)
-    assert len(result) == 1626
+    assert len(result.strip()) == 1625
 
     result = model.as_rdf_string()
     assert isinstance(result, str)
