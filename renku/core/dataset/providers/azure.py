2dad2494d4f7a7bfb1cc6f4effd85674a1781856
Mohammad Alisafaee <mohammad.alisafaee@epfl.ch>
2022-12-21 20:23:18 +0100

    feat(dataset): support for azure blob storage

--- renku/core/dataset/providers/azure.py
+++ renku/core/dataset/providers/azure.py
@@ -15,7 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""S3 dataset provider."""
+"""Azure dataset provider."""
 
 import re
 import urllib
@@ -33,7 +33,7 @@ from renku.core.dataset.providers.api import (
 )
 from renku.core.dataset.providers.models import DatasetAddAction, DatasetAddMetadata, ProviderParameter
 from renku.core.interface.storage import IStorage, IStorageFactory
-from renku.core.util.metadata import prompt_for_credentials
+from renku.core.util.metadata import get_canonical_key, prompt_for_credentials
 from renku.core.util.urls import get_scheme
 from renku.domain_model.dataset import RemoteEntity
 from renku.domain_model.project_context import project_context
@@ -42,24 +42,25 @@ if TYPE_CHECKING:
     from renku.domain_model.dataset import Dataset
 
 
-class S3Provider(ProviderApi, AddProviderInterface, StorageProviderInterface):
-    """S3 provider."""
+class AzureProvider(ProviderApi, AddProviderInterface, StorageProviderInterface):
+    """Azure provider."""
 
     priority = ProviderPriority.HIGHEST
-    name = "S3"
+    name = "Azure"
 
     def __init__(self, uri: Optional[str]):
         super().__init__(uri=uri)
 
-        endpoint, bucket, _ = parse_s3_uri(uri=self.uri)
+        account, endpoint, container, _ = parse_azure_uri(uri=self.uri)
 
-        self._bucket: str = bucket
+        self._account: str = account
         self._endpoint: str = endpoint
+        self._container = container
 
     @staticmethod
     def supports(uri: str) -> bool:
         """Whether or not this provider supports a given URI."""
-        return get_scheme(uri) == "s3"
+        return get_scheme(uri) == "azure"
 
     @staticmethod
     def get_add_parameters() -> List["ProviderParameter"]:
@@ -71,49 +72,48 @@ class S3Provider(ProviderApi, AddProviderInterface, StorageProviderInterface):
                 "storage",
                 flags=["storage"],
                 default=None,
-                help="Uri for the S3 bucket when creating the dataset at the same time when running 'add'",
+                help="Uri for the Azure container when creating the dataset at the same time when running 'add'",
                 multiple=False,
                 type=str,
             ),
         ]
 
-    def get_credentials(self) -> "S3Credentials":
+    def get_credentials(self) -> "AzureCredentials":
         """Return an instance of provider's credential class."""
-        return S3Credentials(provider=self)
+        return AzureCredentials(provider=self)
 
     @inject.autoparams("storage_factory")
     def get_storage(
         self, storage_factory: "IStorageFactory", credentials: Optional["ProviderCredentials"] = None
     ) -> "IStorage":
         """Return the storage manager for the provider."""
-        s3_configuration = {
-            "type": "s3",
-            "provider": "AWS",
+        azure_configuration = {
+            "type": "azureblob",
             "endpoint": self.endpoint,
         }
 
-        def create_renku_storage_s3_uri(uri: str) -> str:
-            """Create a S3 URI to work with the Renku storage handler."""
-            _, bucket, path = parse_s3_uri(uri=uri)
+        def create_renku_storage_azure_uri(uri: str) -> str:
+            """Create an Azure URI to work with the Renku storage handler."""
+            _, _, container, path = parse_azure_uri(uri=uri)
 
-            return f"s3://{bucket}/{path}"
+            return f"azure://{container}/{path}"
 
         if not credentials:
             credentials = self.get_credentials()
             prompt_for_credentials(credentials)
 
         return storage_factory.get_storage(
-            storage_scheme="s3",
+            storage_scheme="azure",
             provider=self,
             credentials=credentials,
-            configuration=s3_configuration,
-            uri_convertor=create_renku_storage_s3_uri,
+            configuration=azure_configuration,
+            uri_convertor=create_renku_storage_azure_uri,
         )
 
     def add(self, uri: str, destination: Path, **kwargs) -> List["DatasetAddMetadata"]:
         """Add files from a URI to a dataset."""
         if re.search(r"[*?]", uri):
-            raise errors.ParameterError("Wildcards like '*' or '?' are not supported for S3 URIs.")
+            raise errors.ParameterError("Wildcards like '*' or '?' are not supported for Azure URIs.")
 
         storage = self.get_storage()
 
@@ -134,14 +134,19 @@ class S3Provider(ProviderApi, AddProviderInterface, StorageProviderInterface):
         ]
 
     @property
-    def bucket(self) -> str:
-        """Return S3 bucket name."""
-        return self._bucket
+    def account(self) -> str:
+        """Azure account name."""
+        return self._account
 
     @property
     def endpoint(self) -> str:
-        """Return S3 bucket endpoint."""
+        """Return Azure container endpoint."""
         return self._endpoint
+
+    @property
+    def container(self) -> str:
+        """Return Azure container name."""
+        return self._container
 
     def on_create(self, dataset: "Dataset") -> None:
         """Hook to perform provider-specific actions on a newly-created dataset."""
@@ -149,50 +154,59 @@ class S3Provider(ProviderApi, AddProviderInterface, StorageProviderInterface):
         prompt_for_credentials(credentials)
         storage = self.get_storage(credentials=credentials)
 
-        # NOTE: The underlying rclone tool cannot tell if a directory within a S3 bucket exists or not
+        # NOTE: The underlying rclone tool cannot tell if a directory within an Azure container exists or not
         if not storage.exists(self.uri):
-            raise errors.ParameterError(f"S3 bucket '{self.bucket}' doesn't exists.")
+            raise errors.ParameterError(f"Azure container '{self.container}' doesn't exists.")
 
         project_context.repository.add_ignored_pattern(pattern=str(dataset.get_datadir()))
 
 
-class S3Credentials(ProviderCredentials):
-    """S3-specific credentials."""
+class AzureCredentials(ProviderCredentials):
+    """Azure-specific credentials."""
 
-    def __init__(self, provider: S3Provider):
+    def __init__(self, provider: AzureProvider):
         super().__init__(provider=provider)
+
+        # NOTE: Set account name so that users don't need to re-enter it
+        self.data[get_canonical_key("Account")] = self.provider.account
 
     @staticmethod
     def get_credentials_names() -> Tuple[str, ...]:
         """Return a tuple of the required credentials for a provider."""
-        return "Access Key ID", "Secret Access Key"
+        return "Account", "Key"
 
     @property
-    def provider(self) -> S3Provider:
+    def provider(self) -> AzureProvider:
         """Return the associated provider instance."""
-        return cast(S3Provider, self._provider)
+        return cast(AzureProvider, self._provider)
 
     def get_credentials_section_name(self) -> str:
         """Get section name for storing credentials.
 
         NOTE: This methods should be overridden by subclasses to allow multiple credentials per providers if needed.
         """
-        return self.provider.endpoint.lower()
+        return f"{self.provider.account}.{self.provider.endpoint}"
 
 
-def parse_s3_uri(uri: str) -> Tuple[str, str, str]:
-    """Extract endpoint, bucket name, and path within the bucket from a given URI.
+def parse_azure_uri(uri: str) -> Tuple[str, str, str, str]:
+    """Extract account, endpoint, container, and path within the container from a given URI.
 
-    NOTE: We only support s3://<endpoint>/<bucket-name>/<path> at the moment.
+    NOTE: We support azure://<account-name>.<endpoint>/<container-name>/<path> or
+    azure://<account-name>/<container-name>/<path>.
     """
     parsed_uri = urllib.parse.urlparse(uri)
 
-    endpoint = parsed_uri.netloc
+    account, _, endpoint = parsed_uri.netloc.partition(".")
+
+    if parsed_uri.scheme.lower() != "azure" or not account:
+        raise errors.ParameterError(
+            f"Invalid Azure URI: {uri}. Valid format is 'azure://<account-name>.<endpoint>/<container-name>/<path>' or "
+            "azure://<account-name>/<container-name>/<path>"
+        )
+
+    endpoint = endpoint.lower() or "blob.core.windows.net"
+
     path = parsed_uri.path.strip("/")
+    container, _, path = path.partition("/")
 
-    if parsed_uri.scheme.lower() != "s3" or not endpoint:
-        raise errors.ParameterError(f"Invalid S3 URI: {uri}. Valid format is 's3://<endpoint>/<bucket-name>/<path>'")
-
-    bucket, _, path = path.partition("/")
-
-    return endpoint, bucket, path.strip("/")
+    return account, endpoint, container, path.strip("/")
