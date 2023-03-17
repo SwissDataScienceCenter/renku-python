0e116b108cd2a2c149e2351d058eb3dea24f0d40
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2023-03-17 16:03:14 +0100

    fix additional test

--- renku/infrastructure/gateway/dataset_gateway.py
+++ renku/infrastructure/gateway/dataset_gateway.py
@@ -71,9 +71,10 @@ class DatasetGateway(IDatasetGateway):
                 tags.remove(t)
                 break
 
-    @deal.pre(
-        lambda _: _.dataset.date_created is None or _.dataset.date_created >= project_context.project.date_created
-    )
+    # NOTE: Enable this again once we properly deal with `date_created` on imported Renku datasets
+    # @deal.pre(
+    #     lambda _: _.dataset.date_created is None or _.dataset.date_created >= project_context.project.date_created
+    # )
     def add_or_remove(self, dataset: Dataset) -> None:
         """Add or remove a dataset."""
         database = project_context.database
