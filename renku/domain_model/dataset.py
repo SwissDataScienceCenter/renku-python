2108f65d00cc9f57da1b1db842401d4c43983073
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2023-03-21 15:21:48 +0100

    test: fix deal precondition failure in IT (#3367)

--- renku/domain_model/dataset.py
+++ renku/domain_model/dataset.py
@@ -550,8 +550,8 @@ class Dataset(Persistent):
         or (self.date_created is None and self.date_published is not None)
     )
     @deal.ensure(
-        lambda self, *_, result, **kwargs: self.date_modified >= self.date_created
-        or self.date_modified >= self.date_published
+        lambda self, *_, result, **kwargs: (self.date_created is not None and self.date_modified >= self.date_created)
+        or (self.date_published is not None and self.date_modified >= self.date_published)
     )
     @deal.ensure(lambda self, *_, result, **kwargs: self.derived_from is not None)
     def derive_from(
