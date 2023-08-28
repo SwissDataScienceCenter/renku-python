88ff0654e2f5775e994790b48ba4fe70208a2fa2
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2023-08-28 18:47:49 +0200

    squashme: add tests

---
+++ tests/service/cache/test_project_model.py
@@ -0,0 +1,17 @@
+import pytest
+
+from renku.ui.service.cache.models.project import Project, NO_BRANCH_FOLDER, DETACHED_HEAD_FOLDER_PREFIX
+
+
+@pytest.mark.parametrize(
+    "commit_sha,branch,expected_folder",
+    [
+        (None, None, NO_BRANCH_FOLDER),
+        ("commit_sha", None, f"{DETACHED_HEAD_FOLDER_PREFIX}commit_sha"),
+        (None, "branch", "branch"),
+        (None, "master", "master"),
+    ],
+)
+def test_project_model_path(commit_sha, branch, expected_folder):
+    project = Project(name="name", slug="slug", commit_sha=commit_sha, user_id="user_id", owner="owner", branch=branch)
+    assert project.abs_path.stem == expected_folder
