b4f137baff6916688e95a02ca1791864db6c102b
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2023-09-28 17:08:14 +0200

    fix tests

---
+++ tests/fixtures/storage.py
@@ -0,0 +1,60 @@
+# Copyright Swiss Data Science Center (SDSC)
+# A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+# Eidgenössische Technische Hochschule Zürich (ETHZ).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Storage fixtures."""
+from renku.core.interface.storage_service_gateway import IStorageService
+from renku.domain_model.cloud_storage import CloudStorage, CloudStorageWithSensitiveFields
+
+
+class DummyStorageService(IStorageService):
+    """Dummy storage service."""
+
+    @property
+    def project_id(self):
+        """Get a dummy project id."""
+        return "123456"
+
+    def list(self, project_id):
+        """List dummy storage definition."""
+        return [
+            CloudStorageWithSensitiveFields(
+                CloudStorage(
+                    name="mystorage",
+                    source_path="source",
+                    target_path="target/path",
+                    private=False,
+                    configuration={"type": "s3", "endpoint": "example.com"},
+                    storage_id="ABCDEFG",
+                    project_id="123456",
+                ),
+                [],
+            )
+        ]
+
+    def create(self, storage):
+        """Create storage."""
+        raise NotImplementedError()
+
+    def edit(self, storage_id, storage):
+        """Edit storage."""
+        raise NotImplementedError()
+
+    def delete(self, storage_id):
+        """Delete storage."""
+        raise NotImplementedError()
+
+    def validate(self, storage):
+        """Validate storage."""
+        raise NotImplementedError()
--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,4 @@
-#
-# Copyright 2020-2023 -Swiss Data Science Center (SDSC)
+# Copyright Swiss Data Science Center (SDSC)
 # A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
 # Eidgenössische Technische Hochschule Zürich (ETHZ).
 #
@@ -166,11 +165,13 @@ def get_test_bindings() -> Tuple[Dict, Dict[Type, Callable[[], Any]]]:
     from renku.core.interface.dataset_gateway import IDatasetGateway
     from renku.core.interface.plan_gateway import IPlanGateway
     from renku.core.interface.project_gateway import IProjectGateway
+    from renku.core.interface.storage_service_gateway import IStorageService
     from renku.infrastructure.gateway.activity_gateway import ActivityGateway
     from renku.infrastructure.gateway.database_gateway import DatabaseGateway
     from renku.infrastructure.gateway.dataset_gateway import DatasetGateway
     from renku.infrastructure.gateway.plan_gateway import PlanGateway
     from renku.infrastructure.gateway.project_gateway import ProjectGateway
+    from tests.fixtures.storage import DummyStorageService
 
     constructor_bindings = {
         IPlanGateway: lambda: PlanGateway(),
@@ -178,6 +179,7 @@ def get_test_bindings() -> Tuple[Dict, Dict[Type, Callable[[], Any]]]:
         IDatabaseGateway: lambda: DatabaseGateway(),
         IDatasetGateway: lambda: DatasetGateway(),
         IProjectGateway: lambda: ProjectGateway(),
+        IStorageService: lambda: DummyStorageService(),
     }
 
     return {}, constructor_bindings
